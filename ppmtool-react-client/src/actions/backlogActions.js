d576d61dc7d2dc65940580adcf1c544f7ea8ab50
Nikhil <nikekr7@gmail.com>
2023-08-21 19:31:38 +0530

    backend proxy

--- ppmtool-react-client/src/actions/backlogActions.js
+++ ppmtool-react-client/src/actions/backlogActions.js
@@ -4,7 +4,7 @@ import { GET_ERRORS, GET_BACKLOG, GET_PROJECT_TASK, DELETE_PROJECT_TASK } from "
 export const addProjectTask =
   (backlog_id, project_task, history) => async (dispatch) => {
     try {
-      await axios.post(`https://project-track.onrender.com/api/backlog/${backlog_id}`, project_task);
+      await axios.post(`https://ppmtool.onrender.com/api/backlog/${backlog_id}`, project_task);
       history.push(`/projectBoard/${backlog_id}`);
       dispatch({
         type: GET_ERRORS,
@@ -20,7 +20,7 @@ export const addProjectTask =
 
 export const getBacklog = (backlog_id) => async (dispatch) => {
   try {
-    const res = await axios.get(`https://project-track.onrender.com/api/backlog/${backlog_id}`);
+    const res = await axios.get(`https://ppmtool.onrender.com/api/backlog/${backlog_id}`);
     dispatch({
       type: GET_BACKLOG,
       payload: res.data,
@@ -36,7 +36,7 @@ export const getBacklog = (backlog_id) => async (dispatch) => {
 export const getProjectTask =
   (backlog_id, pt_id, history) => async (dispatch) => {
     try {
-      const res = await axios.get(`https://project-track.onrender.com/api/backlog/${backlog_id}/${pt_id}`);
+      const res = await axios.get(`https://ppmtool.onrender.com/api/backlog/${backlog_id}/${pt_id}`);
       dispatch({
         type: GET_PROJECT_TASK,
         payload: res.data,
@@ -49,7 +49,7 @@ export const getProjectTask =
 export const updateProjectTask =
   (backlog_id, pt_id, project_task, history) => async (dispatch) => {
     try {
-      axios.patch(`https://project-track.onrender.com/api/backlog/${backlog_id}/${pt_id}`, project_task);
+      axios.patch(`https://ppmtool.onrender.com/api/backlog/${backlog_id}/${pt_id}`, project_task);
       history.push(`/projectBoard/${backlog_id}`);
     } catch (err) {
       dispatch({
@@ -61,7 +61,7 @@ export const updateProjectTask =
 
 export const deleteProjectTask = (backlog_id, pt_id) => async dispatch =>{
   if(window.confirm(`You Are deleting Project Task ${pt_id}`)){
-    await axios.delete(`https://project-track.onrender.com/api/backlog/${backlog_id}/${pt_id}`)
+    await axios.delete(`https://ppmtool.onrender.com/api/backlog/${backlog_id}/${pt_id}`)
     dispatch({
       type: DELETE_PROJECT_TASK,
       payload: pt_id
--- ppmtool-react-client/src/actions/projectActions.js
+++ ppmtool-react-client/src/actions/projectActions.js
@@ -4,7 +4,7 @@ import { GET_ERRORS, GET_PROJECT, GET_PROJECTS, DELETE_PROJECT } from "./types"
 
 export const createProject = (project, history) => async dispatch =>{
     try {
-        await axios.post("https://project-track.onrender.com/api/project", project)
+        await axios.post("https://ppmtool.onrender.com/api/project", project)
         history.push("/dashboard")
         dispatch({
             type: GET_ERRORS,
@@ -19,7 +19,7 @@ export const createProject = (project, history) => async dispatch =>{
 };
 
 export const getProjects = () => async dispatch =>{
-    const res= await axios.get("https://project-track.onrender.com/api/project/all")
+    const res= await axios.get("https://ppmtool.onrender.com/api/project/all")
     dispatch({
         type:GET_PROJECTS, 
         payload: res.data
@@ -29,7 +29,7 @@ export const getProjects = () => async dispatch =>{
 
 export const getProject = (id, history) => async dispatch => {
     try {
-        const res = await axios.get(`https://project-track.onrender.com/api/project/${id}`);
+        const res = await axios.get(`https://ppmtool.onrender.com/api/project/${id}`);
         dispatch({
             type:GET_PROJECT,
             payload:res.data
@@ -41,7 +41,7 @@ export const getProject = (id, history) => async dispatch => {
 
 export const deleteProject = id => async dispatch =>{
     if(window.confirm("Are you Sure to Delete the Project?")){
-        await axios.delete(`https://project-track.onrender.com/api/project/${id}`)
+        await axios.delete(`https://ppmtool.onrender.com/api/project/${id}`)
         dispatch({
             type:DELETE_PROJECT,
             payload:id
